b21bbbbcec08a7414aea7da5e9ea853faa93f338
madhu.uppu <madhu.uppu.12@otsi.co.in>
2021-07-13 00:09:23 -0500

    remove duplicates

--- src/main/FindDuplicateStringsInWord.java
+++ src/main/FindDuplicateStringsInWord.java
@@ -1,11 +1,13 @@
 package main;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class FindDuplicateStringsInWord {
 
 	public static void main(String[] args) {
-
-		String[] stringArray = "one two two three three three four four four four one five six six".split(" ");
-
+		String s = "one two two three three three four four four four one five six six";
+		String[] stringArray = s.split(" ");
 		int count = 1;
 		for (Integer i = 0; i < stringArray.length; i++) {
 			if (stringArray[i].equals(" ")) {
@@ -23,6 +25,16 @@ public class FindDuplicateStringsInWord {
 			stringArray[i] = " ";
 			count = 1;
 		}
+		stringArray = s.split(" ");
+		HashMap<String, Integer> hm = new LinkedHashMap<String, Integer>();
+		for (Integer i = 0; i < stringArray.length; i++) {
+			if (hm.containsKey(stringArray[i])) {
+				hm.put(stringArray[i], hm.get(stringArray[i]) + 1);
+			} else {
+				hm.put(stringArray[i], 1);
+			}
+		}
+		hm.entrySet().stream().forEach(c -> System.out.println(" " + c.getKey() + "  " + c.getValue()));
 
 	}
 
---
+++ src/main/RemoveDuplicateStringsFromWord.java
@@ -0,0 +1,27 @@
+package main;
+
+public class RemoveDuplicateStringsFromWord {
+
+	public static void main(String[] args) {
+		String s = "one two two three three three four four four four one five six six";
+		String[] stringArray = s.split(" ");
+		System.out.println("Original String " + s);
+		System.out.println("After Duplicates Removal ");
+		int count = 1;
+		for (int i = 0; i < stringArray.length; i++) {
+			if (stringArray[i].equals(" ")) {
+				continue;
+			}
+			for (Integer j = i + 1; j < stringArray.length; j++) {
+
+				if (stringArray[i].equals(stringArray[j])) {
+					count = count + 1;
+					if (count > 1) {
+						stringArray[j] = " ";
+					}
+				}
+			}
+			System.out.print(" "+stringArray[i]);
+		}
+	}
+}
